93b574558d3f108a43b844ab41777f8b04bd917c
Putu Mas Mertayasa <putumas.mertayasa.e@thalesdigital.io>
2021-05-26 20:43:01 +0800

    Integrate with elastic search

--- src/main/java/com/samutup/kafka/consumer/KafkaConsumerVerticle.java
+++ src/main/java/com/samutup/kafka/consumer/KafkaConsumerVerticle.java
@@ -6,17 +6,23 @@ import com.samutup.kafka.liveready.ServerStartupListener;
 import com.samutup.kafka.liveready.ServerStartupListenerImpl;
 import com.samutup.kafka.settings.TweetySetting;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.reactiverse.elasticsearch.client.reactivex.RestHighLevelClient;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.Json;
+import io.vertx.core.net.SocketAddress;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
 import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
@@ -45,6 +51,11 @@ public class KafkaConsumerVerticle extends AbstractVerticle {
     return KafkaConsumer.create(vertx, config);
   }
 
+  @FunctionalInterface
+  interface Transform<R, T, C> {
+
+    C apply(R clientBuilder, T t);
+  }
 
   private Handler<RoutingContext> contextHandler = rc -> {
 
@@ -60,12 +71,43 @@ public class KafkaConsumerVerticle extends AbstractVerticle {
 
   };
 
+  private static Handler<RoutingContext> handleAdminRequest(
+      WebClient webClient, TweetySetting tweetySetting) {
+    return rc -> {
+
+      if (HttpMethod.DELETE.equals(rc.request().method())) {
+        rc.response().setStatusCode(HttpResponseStatus.OK.code()).end();
+      } else if (HttpMethod.POST.equals(rc.request().method())) {
+        //get elastic search
+        HttpRequest<Buffer> httpRequest = webClient.request(HttpMethod.PUT, SocketAddress
+                .inetSocketAddress(tweetySetting.getElasticPort(), tweetySetting.getElasticHost()),
+            rc.request().uri());
+        httpRequest.sendJson(Json.decodeValue(rc.getBodyAsString()))
+            .onSuccess(bufferHttpResponse -> LOGGER.info(
+                "OK " + bufferHttpResponse.statusCode() + bufferHttpResponse.statusMessage()
+                    + bufferHttpResponse.bodyAsString()));
+        rc.response().setStatusCode(HttpResponseStatus.CREATED.code()).end();
+      } else if (HttpMethod.GET.equals(rc.request().method())) {
+        HttpRequest<Buffer> httpRequest = webClient.request(HttpMethod.GET, SocketAddress
+                .inetSocketAddress(tweetySetting.getElasticPort(), tweetySetting.getElasticHost()),
+            rc.request().uri());
+        Future<HttpResponse<Buffer>> responseFuture = httpRequest.send();
+        responseFuture.onSuccess(res -> rc.response().putHeader("Content-Type","application/json").setStatusCode(res.statusCode()).end(res.bodyAsString()))
+            .onFailure(res->rc.response().setStatusCode(500).end(res.getMessage()));
+      } else {
+        rc.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end();
+      }
+
+    };
+
+  }
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     Router router = Router.router(vertx);
     try {
       TweetySetting tweetySetting = config().mapTo(TweetySetting.class);
-
+      WebClient webClient = WebClient.create(vertx);
       LOGGER.info("retrieve settings from yaml " + tweetySetting);
       int portNumber = tweetySetting.getPort();
       //list all path
@@ -81,7 +123,8 @@ public class KafkaConsumerVerticle extends AbstractVerticle {
       // create server
       HttpServer server = vertx.createHttpServer();
       // tweetListener.listen(kafkaProducer, tweetySetting.getTopicName());
-      router.route().handler(BodyHandler.create()).handler(contextHandler);
+      router.route().handler(BodyHandler.create())
+          .handler(handleAdminRequest(webClient, tweetySetting));
       server.requestHandler(router).listen(portNumber, serverStartupListenHandler);
     } catch (Exception exception) {
       LOGGER.error("Unexpected error, config " + config(), exception);
