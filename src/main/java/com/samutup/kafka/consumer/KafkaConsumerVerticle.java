bd90d0937513d05063d01a41cb4969e73e99c58b
Putu Mas Mertayasa <putumas.mertayasa.e@thalesdigital.io>
2021-05-27 00:22:32 +0800

    Integrate with elastic search and kafka

--- src/main/java/com/samutup/kafka/consumer/KafkaConsumerVerticle.java
+++ src/main/java/com/samutup/kafka/consumer/KafkaConsumerVerticle.java
@@ -7,7 +7,6 @@ import com.samutup.kafka.liveready.ServerStartupListenerImpl;
 import com.samutup.kafka.settings.TweetySetting;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
@@ -18,7 +17,6 @@ import io.vertx.core.http.RequestOptions;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.Json;
-import io.vertx.core.net.SocketAddress;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.HttpRequest;
@@ -49,11 +47,11 @@ public class KafkaConsumerVerticle extends AbstractVerticle {
     return KafkaConsumer.create(vertx, config);
   }
 
-  Handler<HttpResponse<Buffer>> httpResponseHandler = bufferHttpResponse -> LOGGER
+  static Handler<HttpResponse<Buffer>> httpResponseHandler = bufferHttpResponse -> LOGGER
       .info(bufferHttpResponse.bodyAsString());
-  Handler<Throwable> throwableHandler = throwable -> LOGGER.error("error", throwable);
+  static Handler<Throwable> throwableHandler = throwable -> LOGGER.error("error", throwable);
 
-  private void consumeTopic(HttpRequest<Buffer> bufferHttpRequest,
+  private static void consumeTopic(HttpRequest<Buffer> bufferHttpRequest,
       KafkaConsumer<String, String> consumer) {
     new Thread(() -> {
       while (true) {
@@ -70,29 +68,25 @@ public class KafkaConsumerVerticle extends AbstractVerticle {
   }
 
   private static Handler<RoutingContext> handleAdminRequest(
-      WebClient webClient, TweetySetting tweetySetting) {
+      HttpRequest<Buffer> httpRequest, TweetySetting tweetySetting) {
     return rc -> {
 
       if (HttpMethod.DELETE.equals(rc.request().method())) {
         rc.response().setStatusCode(HttpResponseStatus.OK.code()).end();
       } else if (HttpMethod.POST.equals(rc.request().method())) {
         //get elastic search
-        HttpRequest<Buffer> httpRequest = webClient.request(HttpMethod.PUT, SocketAddress
-                .inetSocketAddress(tweetySetting.getElasticPort(), tweetySetting.getElasticHost()),
-            rc.request().uri());
-        httpRequest.sendJson(Json.decodeValue(rc.getBodyAsString()))
-            .onSuccess(bufferHttpResponse -> LOGGER.info(
-                "OK " + bufferHttpResponse.statusCode() + bufferHttpResponse.statusMessage()
-                    + bufferHttpResponse.bodyAsString()));
+        KafkaConsumer<String, String> consumer = kafkaConsumerBuilder(rc.vertx(), tweetySetting);
+        consumer.subscribe(tweetySetting.getTopicName());
+        consumeTopic(httpRequest, consumer);
         rc.response().setStatusCode(HttpResponseStatus.CREATED.code()).end();
       } else if (HttpMethod.GET.equals(rc.request().method())) {
-        HttpRequest<Buffer> httpRequest = webClient.request(HttpMethod.GET, SocketAddress
+        /*HttpRequest<Buffer> httpRequest = webClient.request(HttpMethod.GET, SocketAddress
                 .inetSocketAddress(tweetySetting.getElasticPort(), tweetySetting.getElasticHost()),
             rc.request().uri());
         Future<HttpResponse<Buffer>> responseFuture = httpRequest.send();
         responseFuture.onSuccess(res -> rc.response().putHeader("Content-Type", "application/json")
             .setStatusCode(res.statusCode()).end(res.bodyAsString()))
-            .onFailure(res -> rc.response().setStatusCode(500).end(res.getMessage()));
+            .onFailure(res -> rc.response().setStatusCode(500).end(res.getMessage()));*/
       } else {
         rc.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end();
       }
@@ -124,16 +118,13 @@ public class KafkaConsumerVerticle extends AbstractVerticle {
           .registerReadinessCheck(router, new AppCheckHandler[]{serverStartupListenHandler});
       LifenessReadinessCheck.registerLivenessCheck(router, null);
       //call kafka
-      KafkaConsumer<String, String> consumer = kafkaConsumerBuilder(vertx, tweetySetting);
-      consumer.subscribe(tweetySetting.getTopicName());
-      consumeTopic(elasticPutMessage, consumer);
 
       // create server
       HttpServer server = vertx.createHttpServer();
       // tweetListener.listen(kafkaProducer, tweetySetting.getTopicName());
 
       router.route().handler(BodyHandler.create())
-          .handler(handleAdminRequest(webClient, tweetySetting));
+          .handler(handleAdminRequest(elasticPutMessage, tweetySetting));
       server.requestHandler(router).listen(portNumber, serverStartupListenHandler);
     } catch (Exception exception) {
       LOGGER.error("Unexpected error, config " + config(), exception);
